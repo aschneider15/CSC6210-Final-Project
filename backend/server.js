bd23c1558cc1de458e3540b6170dd606227ff0fa
aschneider15 <aschneider15@outlook.com>
2024-10-05 16:06:28 -0500

    Initial Commit
    Created final project for CSC6210. Backend complete, frontend in progress.

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const playerRoutes = require("./routes/players");
+
+const app = express();
+const port = 3000;
+
+mongoose
+  .connect("mongodb://localhost:27017/Packers")
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log("Example app listening on port" + port);
+    });
+  })
+  .catch((err) => console.error("Failed to connect", err));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/players", playerRoutes);
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import PlayerTable from "./components/PlayerTable.jsx";
+
+function App() {
+  return(
+  <>
+    <h1>Green Bay Packers</h1>
+    <h2>Player Information Database</h2>
+    <PlayerTable/>
+  </>
+  );
+}
+
+export default App;
